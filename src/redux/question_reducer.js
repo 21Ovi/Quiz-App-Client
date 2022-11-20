c2c91b27b93f6460a71cbee23438ce074de0d13a
= <=>
2022-11-20 18:33:12 +0530

    redux setup

---
+++ src/redux/question_reducer.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const questionReducer = createSlice({
+  name: "questions",
+  initialState: {
+    queue: [],
+    answers: [],
+    trace: 0,
+  },
+  reducers: {
+    startExamAction: (state, action) => {
+      return {
+        ...state,
+        queue: action.payload,
+      };
+    },
+  },
+});
+
+export const { startExamAction } = questionReducer.actions;
+
+export default questionReducer.reducer;
