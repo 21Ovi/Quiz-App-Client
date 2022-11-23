b5111d37a8c00c438240a276bd76e6b2f20d2e0d
= <=>
2022-11-24 00:59:56 +0530

    Integrating APIs

--- src/helper/helper.js
+++ src/helper/helper.js
@@ -1,5 +1,6 @@
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
+import axios from "axios";
 
 export const attempts_Number = (result) => {
   return result.filter((r) => r !== undefined).length;
@@ -20,4 +21,19 @@ export const flagResult = (totalPoints, earnedPoints) => {
 export const CheckUserExist = ({ children }) => {
   const auth = useSelector((state) => state.result.userId);
   return auth ? children : <Navigate to={"/"} replace={true}></Navigate>;
+};
+
+// Get server data
+export const getServerData = async (url, callback) => {
+  const data = await axios.get(url)?.data;
+  console.log(data);
+  return callback ? callback(data) : data;
+};
+// getServerData("http://localhost:5000/api/questions");
+
+// Post server data
+export const postServerData = async (url, result, callback) => {
+  const data = await axios.post(url, result)?.data;
+  console.log(data);
+  return callback ? callback(data) : data;
 };
